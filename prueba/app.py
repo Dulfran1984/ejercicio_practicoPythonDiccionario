64afb82b870858315c63e3b664189ebd92485250
Dulfran Antonio Montaño Montaño <126972569+Dulfran1984@users.noreply.github.com>
2024-05-10 17:34:51 -0500

    Actualización 10-05-2024_5:34pm

--- prueba/app.py
+++ prueba/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
-
+from datetime import datetime
+import os
 app = Flask(__name__)
 datos = []
-
+registro = 0
 @app.route('/', methods=['GET', 'POST'])
 def formulario():
     mensaje = ""
@@ -12,14 +13,19 @@ def formulario():
         contacto = request.form['contacto']
         correo = request.form['correo']
         direccion = request.form['direccion']
-        datos.append({'Nombre': nombre, 'Contacto': contacto, 'Correo': correo, 'Direccion': direccion})
+        _archivo = request.files['txtImagen']
         # Validar si se proporcionó algún dato
         mensaje = "Formulario enviado correctamente"
-    
+        tiempo = datetime.now()
+        hora_Actual = tiempo.strftime('%Y%M%H%M%S')
+        if _archivo.filename!="":
+            nombre_Archivo = hora_Actual + "_" +_archivo.filename
+            _archivo.save('templates/img/'+nombre_Archivo)
+        datos.append({'Registro': (registro + 1),'Nombre': nombre, 'Contacto': contacto, 'Correo': correo, 'Direccion': direccion,'Imagen':nombre_Archivo})
     return render_template('formulario.html', mensaje=mensaje)
 @app.route('/mostrar')
 def fnt_mostrar():
     return render_template('mostrar.html', datos = datos)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1', port=1000)
+    app.run(debug=True,host='127.0.0.1', port=5000)
