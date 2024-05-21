43bda24266ffe7b22157ec819871752618c61b15
Dulfran Antonio Montaño Montaño <126972569+Dulfran1984@users.noreply.github.com>
2024-05-21 18:10:13 -0500

    Actualización 21-05-2024_18:09
    1. se realizo el cargado de la imagen en la tabla.
    2. se coloco el ajuste de la imagen de 100x100
    3. se ajustaron algunas características de diseño.

--- prueba/app.py
+++ prueba/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
 from datetime import datetime
+from rembg import remove
+from PIL import Image
 import os
 app = Flask(__name__)
 datos = []
 registro = 0
 @app.route('/', methods=['GET', 'POST'])
 def formulario():
+    global registro
     mensaje = ""
     if request.method == 'POST':
         # Aquí procesas los datos del formulario
@@ -14,18 +17,27 @@ def formulario():
         correo = request.form['correo']
         direccion = request.form['direccion']
         _archivo = request.files['txtImagen']
+        
         # Validar si se proporcionó algún dato
-        mensaje = "Formulario enviado correctamente"
+        mensaje = f"La persona {nombre} ha sido registrada con éxito"
         tiempo = datetime.now()
         hora_Actual = tiempo.strftime('%Y%M%H%M%S')
         if _archivo.filename!="":
             nombre_Archivo = hora_Actual + "_" +_archivo.filename
-            _archivo.save('templates/img/'+nombre_Archivo)
+            _archivo.save('static/img/'+nombre_Archivo)
+            filepath = 'static/img/'+nombre_Archivo
+            image = Image.open(filepath)
+            image = image.resize((100, 100))  # Redimensionar a 500x500 píxeles
+            image.save(filepath)
+            
         datos.append({'Registro': (registro + 1),'Nombre': nombre, 'Contacto': contacto, 'Correo': correo, 'Direccion': direccion,'Imagen':nombre_Archivo})
+        registro+=1
+        
     return render_template('formulario.html', mensaje=mensaje)
 @app.route('/mostrar')
 def fnt_mostrar():
     return render_template('mostrar.html', datos = datos)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1', port=5000)
+    #app.run(debug=True,host='127.0.0.1', port=5000)
+    app.run(debug=True,host='172.22.50.17', port=5000)
