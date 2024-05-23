59110f59e9d7fb7859088be0c2e3c1f7c10a9052
Dulfran Antonio Montaño Montaño <126972569+Dulfran1984@users.noreply.github.com>
2024-05-23 18:17:28 -0500

    Actualización 23-05-2024_6:17PM
    Conexión con base de datos sqlite.

--- prueba/app.py
+++ prueba/app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 from datetime import datetime
 from rembg import remove
 from PIL import Image
+import sqlite3
 import os
 app = Flask(__name__)
-datos = []
-registro = 0
+#datos = []
+#registro = 0
 @app.route('/', methods=['GET', 'POST'])
 def formulario():
     global registro
@@ -29,15 +30,33 @@ def formulario():
             image = Image.open(filepath)
             image = image.resize((100, 100))  # Redimensionar a 500x500 píxeles
             image.save(filepath)
+            guardarDB(nombre, contacto,correo,direccion,nombre_Archivo)
             
-        datos.append({'Registro': (registro + 1),'Nombre': nombre, 'Contacto': contacto, 'Correo': correo, 'Direccion': direccion,'Imagen':nombre_Archivo})
-        registro+=1
+        #datos.append({'Registro': (registro + 1),'Nombre': nombre, 'Contacto': contacto, 'Correo': correo, 'Direccion': direccion,'Imagen':nombre_Archivo})
+        #registro+=1
         
     return render_template('formulario.html', mensaje=mensaje)
+
+@app.route('/guardar')
+def guardarDB(nombre, contacto, correo, direccion,archivo):
+    conn = sqlite3.connect('static/datos.db')
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS datos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, contacto TEXT, correo TEXT, direccion TEXT, imagen TEXT)")
+    cursor.execute("INSERT INTO datos (nombre, contacto, correo, direccion, imagen) VALUES (?,?,?,?,?)", (nombre, contacto, correo, direccion, archivo))
+    conn.commit()
+    conn.close()
+    return render_template('formulario.html', mensaje="Los datos han sido guardados en la base de datos con éxito")
+    
 @app.route('/mostrar')
 def fnt_mostrar():
+    conexion = sqlite3.connect('static/datos.db ')
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM datos")
+    datos = cursor.fetchall()
+    conexion.close()
     return render_template('mostrar.html', datos = datos)
+    print(datos)
 
 if __name__ == '__main__':
     #app.run(debug=True,host='127.0.0.1', port=5000)
-    app.run(debug=True,host='172.22.50.17', port=5000)
+    app.run(debug=True,host='127.0.0.1', port=5000)
